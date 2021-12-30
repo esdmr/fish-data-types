e86a9703c0b917477644ec148a899fd6ccd9f451
esdmr <esdmr0@gmail.com>
2021-12-30 23:30:41 +0330

    Add missing double dash to string builtin

--- functions/object_get.fish
+++ functions/object_get.fish
@@ -3,6 +3,6 @@ function object_get -d 'Get the value of a property' -a index property
     __assert_obj_exists "$index" || return
 
     set -l ref (__object_var "$index" "$property") || return
-    string join \n $$ref
+    string join \n -- $$ref
     return 0
 end
--- functions/object_keys.fish
+++ functions/object_keys.fish
@@ -3,6 +3,6 @@ function object_keys -d 'List the properties of an object' -a index
     __assert_obj_exists "$index" || return
 
     set -l ref (__object_var "$index") || return
-    string join \n $$ref
+    string join \n -- $$ref
     return 0
 end
