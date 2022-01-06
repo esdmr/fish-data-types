8cdc6f159422a4fcc1a66c6d3009a5bd366d735e
esdmr <esdmr0@gmail.com>
2022-01-06 17:47:44 +0330

    Merge the two lint scripts

--- lint.fish
+++ lint.fish
@@ -6,5 +6,24 @@ for filename in $failed_files
     echo "::error file=$filename,line=1,col=1::fish_indent failed in '$filename'."
 end
 
+for filename in functions/*.fish
+    set -l decl 'function '(basename $filename .fish)
+
+    switch (head -n 1 $filename)
+        case $decl $decl' *'
+            # Valid name. Do nothing.
+
+        case 'function *'
+            #Invalid name.
+            echo "::error file=$filename,line=1,col=10::Function in file '$filename' is not consistent with the file name."
+            set failures (math $failures + 1)
+
+        case '*'
+            #Invalid file.
+            echo "::error file=$filename,line=1,col=10::File '$filename' must have one function declaration, since it is under 'functions' directory."
+            set failures (math $failures + 1)
+    end
+end
+
 echo "There were $failures failures."
 exit $fish_indent_status
